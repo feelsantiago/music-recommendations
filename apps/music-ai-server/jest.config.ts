901ee51b22df9d3beb87f5e637748481eabcede9
Filipe Santiago <porto.santiago@hotmail.com>
2025-09-16 13:13:19 -0300

    add api

---
+++ apps/music-ai-server/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'music-ai-server',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/music-ai-server',
+};
