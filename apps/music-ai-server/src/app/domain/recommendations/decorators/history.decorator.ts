6bab0a1e422b92c0ca2ab57bb607f4084d3768bc
Filipe Santiago <porto.santiago@hotmail.com>
2025-10-16 09:56:24 -0300

    fix: fix history being cleaned because type was undefined

--- apps/music-ai-server/src/app/domain/recommendations/decorators/history.decorator.ts
+++ apps/music-ai-server/src/app/domain/recommendations/decorators/history.decorator.ts
@@ -8,12 +8,11 @@ export const History = createParamDecorator((_, ctx: ExecutionContextHost) => {
   const req = http.getRequest();
   const tags = Option.from<string[]>(req.session.tags).unwrapOr([]);
   const body = Option.from<string[]>(req.body.tags).unwrapOr([]);
-  const type = Option.from<string>(req.body.type).unwrapOr('');
+  const type = Option.from<string>(req.query.type).unwrapOr('');
 
   const history = match(req.session.history)
     .with(P.array({ role: P.any }), () => req.session.history)
     .otherwise(() => []);
-
   const sameTags = body.every((tag) => tags.includes(tag));
   const sameType = type === req.session.type;
 
