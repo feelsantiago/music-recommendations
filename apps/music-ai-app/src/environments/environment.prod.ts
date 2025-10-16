050fdd12d54ad20423d20e3664887e03395b90c3
Filipe Santiago <porto.santiago@hotmail.com>
2025-10-16 18:04:30 -0300

    chore: update prod url for music-app

--- apps/music-ai-app/src/environments/environment.prod.ts
+++ apps/music-ai-app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { Env } from './env';
 
 export const environment: Env = {
-  api: 'https://music-recommendations-1082143139606.southamerica-east1.run.app',
+  api: 'https://music-ai-server-1082143139606.southamerica-east1.run.app',
   csrf: {
     cookie: '__Host-psifi.x-csrf-token',
     header: 'x-csrf-token',
