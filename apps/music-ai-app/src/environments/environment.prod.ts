d54b11d6038195f815e5fc26f28cf22fa67f67ee
Filipe Santiago <porto.santiago@hotmail.com>
2025-10-16 16:08:02 -0300

    feat: add api prod url

--- apps/music-ai-app/src/environments/environment.prod.ts
+++ apps/music-ai-app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { Env } from './env';
 
 export const environment: Env = {
-  api: '',
+  api: 'https://music-recommendations-1082143139606.southamerica-east1.run.app',
   csrf: {
     cookie: '__Host-psifi.x-csrf-token',
     header: 'x-csrf-token',
