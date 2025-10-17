e628d62e718bcfc58ab2f36dfbb225fe1246cfd2
Filipe Santiago <porto.santiago@hotmail.com>
2025-10-17 12:38:02 -0300

    chore: use http for back-end calls while certicate is not finish

--- apps/music-ai-app/src/environments/environment.prod.ts
+++ apps/music-ai-app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { Env } from './env';
 
 export const environment: Env = {
-  api: 'https://api.rand-music.net/api',
+  api: 'http://api.rand-music.net/api',
   csrf: {
     cookie: '__Host-psifi.x-csrf-token',
     header: 'x-csrf-token',
