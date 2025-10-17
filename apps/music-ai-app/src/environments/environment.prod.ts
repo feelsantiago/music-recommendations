b3708007caf6e7db4f820df04273e67dbca1a279
Filipe Santiago <porto.santiago@hotmail.com>
2025-10-17 12:23:06 -0300

    chore: change api prod url

--- apps/music-ai-app/src/environments/environment.prod.ts
+++ apps/music-ai-app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { Env } from './env';
 
 export const environment: Env = {
-  api: 'https://music-ai-server-1082143139606.southamerica-east1.run.app/api',
+  api: 'https://api.rand-music.net/api',
   csrf: {
     cookie: '__Host-psifi.x-csrf-token',
     header: 'x-csrf-token',
